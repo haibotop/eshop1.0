838b662ee334e772228e10022de394879e04b6e5
gsj <shengjie_forever@163.com>
2019-07-07 11:05:31 +0800

    商品属性实体类编写，mapper组件编写

--- src/main/java/com/gsj/www/commodity/constant/PropertyInputType.java
+++ src/main/java/com/gsj/www/commodity/constant/PropertyInputType.java
@@ -7,4 +7,16 @@ package com.gsj.www.commodity.constant;
  * @create 2019 - 07 - 05 22:27
  */
 public class PropertyInputType {
+    /**
+     * 多选
+     */
+    public static final Integer MULTIPUT_CHOICE = 1;
+    /**
+     * 输入
+     */
+    public static final Integer INPUT_TEXT = 2;
+
+    private PropertyInputType() {
+
+    }
 }
--- src/main/java/com/gsj/www/commodity/dao/PropertyDAO.java
+++ src/main/java/com/gsj/www/commodity/dao/PropertyDAO.java
@@ -1,5 +1,10 @@
 package com.gsj.www.commodity.dao;
 
+import com.gsj.www.commodity.domain.PropertyDO;
+import com.gsj.www.commodity.domain.PropertyQuery;
+
+import java.util.List;
+
 /**
  * 商品属性管理模块的DAO组件接口
  *
@@ -7,4 +12,29 @@ package com.gsj.www.commodity.dao;
  * @create 2019 - 07 - 05 22:28
  */
 public interface PropertyDAO {
+    /**
+     * 分页查询商品属性
+     * @param propertyQuery 查询条件
+     * @return 商品属性
+     */
+    List<PropertyDO> listPropertiesByPage(PropertyQuery propertyQuery);
+
+    /**
+     * 新增商品属性
+     * @param propertyDO 商品属性DO对象
+     */
+    Boolean saveProperty(PropertyDO propertyDO);
+
+    /**
+     * 根据id查询商品属性
+     * @param id 商品属性id
+     * @return 商品属性
+     */
+    PropertyDO getPropertyById(Long id);
+
+    /**
+     * 更新商品属性
+     * @param propertyDO 商品属性DO对象
+     */
+    Boolean updateProperty(PropertyDO propertyDO);
 }
